62fb9cab8a878ac0e5cdf7d1c0718b5183618cc0
Anugrah <amvanugrah@gmail.com>
2025-03-06 22:02:05 +0530

    User Registration Successfull first commit

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
+import { getUsers , getUser } from "../controllers/user.controller.js";
+
+import  authorize  from "../middlewares/auth.middleware.js";
+
 
 const userRouter = Router();
 
-userRouter.get('/', (req,res)=>{
-    res.send("GET all users");
-})
+userRouter.get('/', getUsers );
 
-userRouter.get('/:id', (req,res)=>{
-    res.send("GET user by id");
-})
+userRouter.get('/:id', authorize, getUser)
 
 userRouter.post('/', (req,res)=>{
     res.send("POST Create user");
